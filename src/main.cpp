8232fadeb5aa7075fa5141d553a74e0556011ee6
maldata <maldata@gmail.com>
2024-11-26 22:49:22 -0600

    Added some loop throttling

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <cmath>
 
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/System/Clock.hpp>
 
 #include "screen.h"
 
@@ -8,9 +10,13 @@ int main(int argc, char* argv[])
 {
     sf::RenderWindow window(sf::VideoMode(640,480), "First window!");
     bubble::Screen* current_screen = new bubble::Screen(window);
+    sf::Clock throttle;
+    float frames_per_second = 60.0;
+    float sec_per_frame = 1.0 / frames_per_second;
 
     while (window.isOpen())
     {
+        throttle.restart();
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -30,5 +36,10 @@ int main(int argc, char* argv[])
         window.clear(sf::Color::Black);
         current_screen->draw();
         window.display();
+
+        // Figure out how long to sleep, then sleep
+        sf::Time iteration_time = throttle.getElapsedTime();
+        sf::Time sleep_sec = sf::seconds(sec_per_frame - iteration_time.asSeconds());
+        sf::sleep(sleep_sec);
     }
 }
